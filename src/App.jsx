8adde3af54bf78c8d27ca57f7688a7057dea9db7
Robert Bailey <r.bail@hotmail.com>
2025-08-17 15:31:10 +0930

    build and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,36 @@ function App() {
      <div>
       <header>
       <h1>Rob B's Hong Kong Photos Aug 2025</h1>
-      <h3>In the first week of August 2025, I went to Hong Kong to see what it is like once again, and do a few things I was originally going to do in Aug/Sept 2019. I went to HK in July 2016, and was originally going there again in Aug/Sept 2019, before the flights were cancelled because of the unrest which popped up, then the Covid shutdown. A few people showed me around during this visit, thank you to them: Ashley Yue, Apple Kayan Cheung from Hong Kong Getaway, and Cosmo from Withlocals.</h3>
+      <h3>In the first week of August 2025, I went to Hong Kong to see what it is like once again, and do a few things I was originally going to do in Aug/Sept 2019. I went to HK in July 2016, and was originally going there again in Aug/Sept 2019, before the flights were cancelled because of the unrest which popped up, then the Covid shutdown. A few people showed me around during this visit, thank you to them: Ashley Yue, Apple Kayan Cheung from Hong Kong Getaway, and Cosmo from Withlocals. It was partly cloudy and rainy sometimes so a few of these photos might be a bit dark.</h3>
       </header>
       
       <div>
   <ul>
     <li>
       <img src="/pic11.jpg" alt="Image 11" />
-      <p>The 777-312 I was on for SIN to HKG. Interesting to me, I wondered for a bit if this was the same mostly-white 777 involved in the turbulence incident on 21 May 2024. But when I later looked up Flightradar24, this aircraft reg was 9V-SWI, and the accident aircraft was 
+      <p>The 777-312 I was on for SIN to HKG. Interesting to me, I wondered for a bit if this was the same mostly-white 777 involved in the turbulence incident on 21 May 2024. But when I later looked up Flightradar24, this aircraft reg was 9V-SWI, and the incident aircraft was 
       9V-SWM.</p>
       <img src="/pic9.jpg" alt="Image 9" />
-      <p>With Apple Cheung on her walking/hiking tour in around the hills of Hong Kong. She is a former flight attendant, she explained, so we chatted for much of it about aircraft, airlines and travel. She and her driver were nice enough to drop me at the Kai Tak park later on after this hike, probably at about the spot where the Kai Tak terminal was, so a kind of mini renactment of Kai Tak airport.</p>
-      
+      <p>With Apple Cheung on her walking/hiking tour in around the hills of Hong Kong. She is a former flight attendant, she explained, so we chatted for much of it about aircraft, airlines and travel. She and her driver were nice enough to drop me at the Kai Tak park later on after this hike, probably at about the spot where the Kai Tak terminal was, so a kind of mini re-enactment of Kai Tak airport.</p>
+      <img src="/pic53.jpg" alt="Image 53" />
+      <img src="/pic54.jpg" alt="Image 54" />
       <p>This walk was mainly around the eastern side of Hong Kong Island, facing towards the South China Sea.</p>
-      <img src="/pic8.jpg" alt="Image 8" />
+      <img src="/pic30a.jpg" alt="Image 30a" />
+      <img src="/pic42.jpg" alt="Image 42" />
+      <p>The buildings to the left are the Hong Kong jail, Cape Collinson Correctional Institution.</p>
+      <img src="/pic32.jpg" alt="Image 32" />
+      <img src="/pic29.jpg" alt="Image 29" />
+      <img src="/pic28.jpg" alt="Image 28" />
       <p>Big Wave Bay Beach</p>
+      
+      <img src="/pic33.jpg" alt="Image 33" />
+      <img src="/pic8.jpg" alt="Image 8" />
+      <p>Big Wave Bay Beach. It was a weekday, so the beach was fairly quiet that morning.</p>
+
       <img src="/pic4.jpg" alt="Image 4" />
       <p>Kai Tak park: a redevelopment on the site of the old Kai Tak airport, interesting to me as an aircraft nerd. There's a few things there now, a cruise ship terminal, sports stadium, apartments, and a walkway/park along where the runway used to be. It includes this kind of model of the old airport.</p>
       <img src="/pic12.jpg" alt="Image 12" />
-      <p>Where I took these photos is about where the runway used to be, from the footage I saw of Kai Tak. The runway, then some grass towards the side edge. There was still a bit of runway left in 2016, I could see from Checkerboard Hill, still being redeveloped.</p>
+      <p>Where I took these photos is about where the runway used to be, from the footage I saw of Kai Tak. The runway, then some grass towards the side edge. There was still a bit of runway left in 2016, I could see from Checkerboard Hill then, when Kai Tak was still being redeveloped.</p>
       <img src="/pic13.jpg" alt="Image 13" />
       <img src="/pic14.jpg" alt="Image 14" />
       <p>A dumpling soup with pork or beef? - I have forgotten what that one was - and pork jowl with noodles. One of the foods I liked trying a couple of times in HK was the pork cheek/jowl, quite crispy. I had a larger plate of pork cheek at another eatery but forgot to take a photo of that one. I remember a Masterchef episode in which a contestant, Chris, cooked a pig's head, and served the cheek/jowl area, so it was interesting to try this in HK.</p>
@@ -39,29 +50,54 @@ function App() {
       <img src="/pic18.jpg" alt="Image 18" />
       <p>With Ashley Yue at the Bad Coffee & Liquor Club, an ironically/humourously-named cocktail bar. It was indeed very nice in there, the drinks and them playing some records. I think I was a bit tired when this photo was taken, probably midnight SA time, so my facial expression is slightly strange/dazed. Ashley was featured on the 2017 "Travel Man" TV show with Richard Ayoade and Jon Hamm, which was how I came to know of her and her food tour. I was originally going to meet up with her in 2019 before that trip had to be cancelled, so it was great to meet up with her 6 years later.</p>
       
-      <p>The Lantau Island gondola cable car. This went over to Ngong Ping and the Po Lin Monastery in the hills, but gave some good views of HK airport along the way for me. HK airport still a frequent hub for cargo 747s.</p>
+      <p>The Lantau Island gondola cable car, part of Cosmo's tour around Lantau Island. This went over to Ngong Ping and the Po Lin Monastery in the hills, but gave some good views of HK airport along the way for me.</p>
+      <img src="/pic38.jpg" alt="Image 38" />
+      <img src="/pic36.jpg" alt="Image 36" />
+      <img src="/pic5.jpg" alt="Image 5" />
+      <p>HK airport still a frequent hub for cargo 747s</p>
+      <img src="/pic35.jpg" alt="Image 35" />
+      <img src="/pic37.jpg" alt="Image 37" />
+      <p>A bit small to see in this photo but I think a Cathay A330 taking off towards the left.</p>
+      <img src="/pic55.jpg" alt="Image 55" />
+      <p>Ngong Ping village with Buddha statue</p>
       <img src="/pic20.jpg" alt="Image 20" />
       <p>At the Po Lin Monastery</p>
       <img src="/pic24.jpg" alt="Image 24" />
-      
       <img src="/pic25.jpg" alt="Image 25" />
-      
       <img src="/pic27.jpg" alt="Image 27" />
       <p>The Po Lin Monastery temple</p>
+      <img src="/pic21.jpg" alt="Image 21" />
+      <p>Inside the Po Lin Monastery temple</p>
+      <img src="/pic22.jpg" alt="Image 22" />
       <img src="/pic1.jpg" alt="Image 1" />
       <p>A kind of artistic photo/view from the Po Lin Monastery with the Buddha statue.</p>
-
-
       <img src="/pic23.jpg" alt="Image 23" />
       <p>The Buddha statue at the Po Lin Monastery. On this occasion, I decided not to go up these stairs, like I did at the Batu Caves in Malaysia, because my legs were somewhat stiff/sore from the previous day's walking up and down hills. So I thought a bit hazardous this time, in case I tripped or something.</p>
-      <img src="/pic2.jpg" alt="Image 2" />
+      <img src="/pic34.jpg" alt="Image 34" />
       <p>Tai O fishing village: the bay area </p>
+      <img src="/pic56.jpg" alt="Image 56" />
+      <img src="/pic39.jpg" alt="Image 39" />
+      <img src="/pic43.jpg" alt="Image 43" />
+      <p>Tai O fishing village</p>
       <img src="/pic19.jpg" alt="Image 19" />
       <p>The Tai O Kwan Tai Temple, said to be around 300 years old according to Cosmo. From looking up via Google and gov.hk, it was "originally built in 1488 - 1505" then "rebuilt in situ in 1741".</p>
       <img src="/pic15.jpg" alt="Image 15" />
       <p>Tai O: inside Kwan Tai Temple</p>
       <img src="/pic16.jpg" alt="Image 16" />
       <p>Tai O: inside Kwan Tai Temple</p>
+      
+      <img src="/pic47.jpg" alt="Image 47" />
+      <p>Tai O: A boat ride around the village</p>
+      <img src="/pic49.jpg" alt="Image 49" />
+      <img src="/pic50.jpg" alt="Image 50" />
+      <img src="/pic51.jpg" alt="Image 51" />
+      <img src="/pic52.jpg" alt="Image 52" />
+      <img src="/pic7.jpg" alt="Image 7" />
+      <p>Tai O: The boat ride continued into the bay/sea, heading a bit towards Macau</p>
+      <img src="/pic6.jpg" alt="Image 6" />
+      <img src="/pic44.jpg" alt="Image 44" />
+      <img src="/pic45.jpg" alt="Image 45" />
+      <img src="/pic46.jpg" alt="Image 46" />
       <img src="/pic3.jpg" alt="Image 3" />
       <p>Tai O: the surrounding mountains </p>
     </li>
